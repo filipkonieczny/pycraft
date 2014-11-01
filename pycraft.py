22a8c6d9072ede92caeed9dd13596ea38d20e750
jhgrng <jhgrng@gmail.com>
2014-11-01 10:19:47 +0100

    Main script skeleton

---
+++ pycraft.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+# TODO: documentation
+# description
+
+
+# imports
+import pyglet
+
+
+# classes
+
+
+# functions
+def main():
+    # TODO: documentation
+    '''
+    '''
+
+    print pyglet.version
+
+
+# run main function
+if __name__ == "__main__":
+    main()
